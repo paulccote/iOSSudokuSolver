50b954c84f871a53376b233c48dd01f3ec1ca167
Paul Cote <pcote@vols.utk.edu>
2020-05-17 10:22:04 -0400

    Initial Commit

---
+++ SudokuSolver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SudokuSolver
+//
+//  Created by Paul Cote on 5/17/20.
+//  Copyright © 2020 Paul Cote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
